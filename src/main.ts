d534c1d5fa9ea0deddd5a36ee28c0854e6eddcff
renqingzhong <fog3211@163.com>
2023-02-18 16:34:40 +0800

    feat: render canvas

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import './style.css'
-import { setupCounter } from './counter'
+import { renderRadar } from './util'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas-radar')!
+const ctx = canvas.getContext('2d')!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const data = [
+  { name: '羁绊', value: 99, color: '#fff', bgColor: '#E0684E' },
+  { name: '玩乐', value: 63, color: '#fff', bgColor: '#2F6EEB' },
+  { name: '默契', value: 55, color: '#fff', bgColor: '#FFB400' },
+  { name: '承诺', value: 50, color: '#fff', bgColor: '#00A4B8' },
+  { name: '亲密', value: 64, color: '#fff', bgColor: '#00985F' },
+  { name: '激情', value: 45, color: '#fff', bgColor: '#FF782D' },
+];
+
+renderRadar(canvas, ctx, {
+  data,
+  maxBoundaryValue: 100,
+  width: 375,
+  height: 300
+})
